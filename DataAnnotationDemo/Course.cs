e79f44c352d15ee6a43e0e4be1529a8a8de73959
Akshay Minhas <Akshay.Minhas@rsystems.com>
2018-10-30 13:10:56 +0530

    Add project files.

---
+++ DataAnnotationDemo/Course.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAnnotationDemo
+{
+    class Course
+    {
+        public Course()
+        {
+            Tags = new HashSet<Tag>();
+        }
+        public int Id { get; set; }
+
+        [Required]                                     //Setting Name column not nullable
+        [MaxLength(255)]                               //maximum character is 255 
+        public string Name { get; set; }
+
+        public int Level { get; set; }
+
+        [Required]              
+        [MaxLength(2000)]
+        public string Description { get; set; }
+
+        public float FullPrice { get; set; }
+
+        public int AuthorId { get; set; }                      //property to name foreign key column
+
+        [ForeignKey("AuthorId")]                               //setting foreign key
+        public virtual Author Author { get; set; }
+        public virtual ICollection<Tag> Tags { get; set; }
+
+    }
+}
---
+++ DataAnnotationDemo/PlutoContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAnnotationDemo
+{
+    class PlutoContext:DbContext
+    {
+        public PlutoContext():base("name=PlutoContext")
+        {
+
+        }
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            //modelBuilder.Entity<Course>()
+            //            .ToTable("course");          //change table name
+
+            //modelBuilder.Entity<Course>()
+            //            .HasKey(a => a.AuthorId);     //set primary key
+
+            //modelBuilder.Entity<Course>()
+            //            .HasKey(a => new { a.AuthorId, a.Author });     //set primary key as composite key
+
+            //modelBuilder.Entity<Course>()
+            //            .Property(a => a.FullPrice)
+            //            .HasColumnName("Price");               //change column name fullprice to price
+
+            //modelBuilder.Entity<Course>()
+            //            .Property(a => a.FullPrice)
+            //            .HasColumnType("varchar");             //change column data type to varchar
+
+            //modelBuilder.Entity<Course>()
+            //            .Property(a => a.FullPrice)
+            //            .HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.None);   //set column number generation none
+
+            //modelBuilder.Entity<Course>()
+            //            .Property(a => a.FullPrice)
+            //            .IsRequired();                       //making column not nullable
+
+            //modelBuilder.Entity<Course>()
+            //            .Property(a => a.Description) 
+            //            .HasMaxLength(2000);              //setting column maximum length to 2000 characters
+
+            //modelBuilder.Entity<Course>()
+            //            .HasMany(a => a.Tags)
+            //            .WithMany(a => a.Courses);         //Many to Many relationship course-tag
+
+            //modelBuilder.Entity<Author>()
+            //            .HasMany(a => a.Courses)
+            //            .WithRequired(a => a.Author);        //one to many realtionship author-course
+
+            
+                         
+
+        }
+    }
+}
